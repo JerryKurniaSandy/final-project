63a768d843369a4c62e297d019e81a370f7196bc
Jerry Kurnia Sandy <jerrykurnias@gmail.com>
2016-05-26 19:19:04 +0700

    conflict clean

--- source/JavaApplication25/src/Gambar/newpackage/JFrame.java
+++ source/JavaApplication25/src/Gambar/newpackage/JFrame.java
@@ -35,11 +35,11 @@ public class JFrame extends javax.swing.JFrame {
         panelGambar1.setLayout(panelGambar1Layout);
         panelGambar1Layout.setHorizontalGroup(
             panelGambar1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 512, Short.MAX_VALUE)
+            .addGap(0, 602, Short.MAX_VALUE)
         );
         panelGambar1Layout.setVerticalGroup(
             panelGambar1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 398, Short.MAX_VALUE)
+            .addGap(0, 419, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
