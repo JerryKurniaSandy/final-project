a7b8c6e154d5db1d1eb91d30bf7330c81278a53a
Jerry Kurnia Sandy <jerrykurnias@gmail.com>
2016-05-27 17:42:35 +0700

    Melengkapi

--- source/JavaApplication25/src/Jenis.java
+++ source/JavaApplication25/src/Jenis.java
@@ -5,12 +5,20 @@ public abstract class Jenis implements HotelMantap {
     protected boolean status;
     protected double diskon;
     Pelanggan pelanggan=new Pelanggan();
+    //implementasi dari method abstrack pada kelas HotelMantap
     @Override
+    //Deklarasi method abstract tampilTpKamar
     public abstract String tampilTpKamar();
+    //implementasi dari method abstrack pada kelas HotelMantap
     @Override
+    //Deklarasi method abstract tampilJmlhKamar
     public abstract int tampilJmlKamar();
+    //implementasi dari method abstrack pada kelas HotelMantap
     @Override
+    //Deklarasi method abstract tampilHarga
     public abstract double tampilHarga();
+    //implementasi dari method abstrack pada kelas HotelMantap
     @Override
+    //Deklarasi pemanggilan method abstract getDiskon
     public abstract double getDiskon();
 }
